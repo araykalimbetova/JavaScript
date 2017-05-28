81826bb9cd5a5e96c2d08ea8c62de83d2dedf5d5
aray <aray.kalimbetova@mail.ru>
2017-05-28 20:41:46 +0600

    Переменные и типы данных

---
+++ script.js
@@ -0,0 +1,9 @@
+var x,y,z;
+//result
+var result;
+x=10.45;
+y='50.5';
+z=21.57;
+y=parseFloat(y);
+result=x+y+z;
+document.write('<p>Результат сложения чисел:' + x + ' + ' + y + ' + ' + z + ' = ' + '<span style="color:red">' + result + '</span></p>');
