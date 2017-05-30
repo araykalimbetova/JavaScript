9f1968036f253531393d5bf7b30b44ebef4f53f9
aray <aray.kalimbetova@mail.ru>
2017-05-30 19:39:05 +0600

    Циклы

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ z=21.57;
 y=parseFloat(y);
 result=x+y+z;
 document.write('<p>Результат сложения чисел:' + x + ' + ' + y + ' + ' + z + ' = ' + '<span style="color:red">' + result + '</span></p>');*/
-var test,result;
+/*var test,result;
 var type;
 test = 10;
 type = typeof(test);
@@ -47,3 +47,46 @@ else{
 	e_m = e_m + "дней";
 }
 alert(e_m);
+*/
+var x = 1024;
+
+while (x > 0) {
+	x = x / 2;
+
+	if (x%2 !=0){
+		continue;
+	}
+	if(x == 0) {
+		break;
+	}
+	document.write(x + "<br>");
+}
+  var color_tr,color_td;
+document.write("<table border='1px'>");
+
+for(var i = 1; i < 10; i++) {
+	
+	if(i == 1) {
+		color_tr = "#c3c3c3";
+	}
+	else {
+		color_tr = "#ffffff";
+	}
+	
+	document.write("<tr style='height:30px;background-color:" + color_tr + "'>");
+
+	for(var j = 1; j < 10; j++) {
+		
+		if(j == 1 || i == 1) {
+			color_td = "#c3c3c3";
+		}
+		else {
+			color_td = "#ffffff";
+		}
+			
+		document.write("<td style='width:30px;background-color:" + color_td + "'>" + i*j + "</td>");
+	}
+	document.write("</tr>");
+}
+
+document.write("</table>");
